ff5fc669aa1a69acd1d2ce0ad0f70189804b760c
VladocV <148562894+VladocV@users.noreply.github.com>
2024-02-29 12:11:31 +0300

    Update HomeWork4.py

--- HomeWork4.py
+++ HomeWork4.py
@@ -17,25 +17,25 @@ B = np.array([[1, 1, 0],[0, 1, 1],[1, 0, 1]])
 print('2, b:\n',np.dot(A, B))
 
 #3:
-def weights_func(X, Y, lr=0.1, epochs=10):
+def weights_func(X, Y, lr=0.5, epochs=10):
     n_samples, n_features = X.shape
     weights = np.zeros(n_features)
     w0 = 0
     for i in range(epochs):
         Y_pred = np.dot(X, weights) + w0
-        print(X.T)
-        weights += 2 * lr * (1 / n_samples) * X.T @ (Y - Y_pred)
+        weights += 2 * lr * (1 / n_samples) * np.dot(X.T, (Y - Y_pred.T))
         w0 += 2 * lr * (1 / n_samples) * np.sum(Y - Y_pred)
     return weights, w0
 
 X = [[1,145],[2,163],[3,240],[3,350],[4,421],[4,397],[5,620]]
-Y = [[80],[170],[100],[220],[200],[270],[500]]
+Y = [80,170,100,220,200,270,500]
 X = np.array(X)
 Y = np.array(Y)
 X = (X - X.mean(axis=0)) / X.std(axis=0)
-X_train = X[:6, :]
-X_test = X[6:, :]
-Y_train = Y[:6]
-Y_test = Y[6:]
+X_train = X[:5, :]
+X_test = X[5:, :]
+Y_train = Y[:5]
+Y_test = Y[5:]
 
-weight, w0 = weights_func(X_train, Y_train, epochs=100)
+weight, w0 = weights_func(X_train, Y_train, epochs=100000)
+print(weight)
